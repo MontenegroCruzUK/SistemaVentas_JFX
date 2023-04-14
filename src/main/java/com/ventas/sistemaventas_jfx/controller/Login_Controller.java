2ee4c6675d1739ba2c10e60c45ce97485b22ae3c
MontenegroCruzUK <montenegrocruz.uk@gmail.com>
2023-04-14 11:52:06 +0100

    asignando valores

--- src/main/java/com/ventas/sistemaventas_jfx/controller/Login_Controller.java
+++ src/main/java/com/ventas/sistemaventas_jfx/controller/Login_Controller.java
@@ -1,9 +1,17 @@
 package com.ventas.sistemaventas_jfx.controller;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXPasswordField;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import io.github.palexdev.materialfx.font.MFXFontIcon;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 
 import javafx.scene.layout.AnchorPane;
 import io.github.palexdev.materialfx.font.MFXFontIcon;
@@ -14,9 +22,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Login_Controller implements Initializable {
-	
 	@FXML
 	private AnchorPane rootPane;
+	
+	@FXML
+	private MFXTextField txt_Username;
+	
+	@FXML
+	private MFXPasswordField txt_Password;
+	
+	@FXML
+	private MFXButton btn_Login;
 	
 	@FXML
 	private HBox hB_Header;
@@ -38,5 +54,9 @@ public class Login_Controller implements Initializable {
 		closeIcon.addEventHandler (javafx.scene.input.MouseEvent.MOUSE_CLICKED, event -> Platform.exit ());
 		minimizeIcon.addEventHandler (MouseEvent.MOUSE_CLICKED,
 			event -> ((Stage) rootPane.getScene ().getWindow ()).setIconified (true));
+	}
+	@FXML
+	void login(ActionEvent event) {
+	
 	}
 }
