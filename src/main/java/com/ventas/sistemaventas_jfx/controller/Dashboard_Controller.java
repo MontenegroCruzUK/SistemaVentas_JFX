d7d7e80cab16e24c806bc5a7227926b4ada73a5b
MontenegroCruzUK <montenegrocruz.uk@gmail.com>
2023-04-18 09:17:00 +0100

    Creada ventana Nueva Venta

--- src/main/java/com/ventas/sistemaventas_jfx/controller/Dashboard_Controller.java
+++ src/main/java/com/ventas/sistemaventas_jfx/controller/Dashboard_Controller.java
@@ -1,24 +1,34 @@
 package com.ventas.sistemaventas_jfx.controller;
 
 import com.ventas.sistemaventas_jfx.ResourcesLoader;
+import io.github.palexdev.materialfx.controls.MFXIconWrapper;
+import io.github.palexdev.materialfx.controls.MFXRectangleToggleNode;
+import io.github.palexdev.materialfx.utils.ScrollUtils;
 import io.github.palexdev.materialfx.utils.ToggleButtonsUtil;
+import io.github.palexdev.materialfx.utils.others.loader.MFXLoader;
+import io.github.palexdev.materialfx.utils.others.loader.MFXLoaderBean;
+import javafx.application.Platform;
+import javafx.css.PseudoClass;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import io.github.palexdev.materialfx.controls.MFXScrollPane;
 import io.github.palexdev.materialfx.font.MFXFontIcon;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 
+import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Dashboard_Controller implements Initializable {
@@ -63,7 +73,66 @@ public class Dashboard_Controller implements Initializable {
 	
 	@Override
 	public void initialize (URL location, ResourceBundle resources) {
+		actionButtonHeader ();
+		
+	}
 	
+	private void actionButtonHeader () {
+		closeIcon.addEventHandler (MouseEvent.MOUSE_CLICKED, event -> Platform.exit ());
+		minimizeIcon.addEventHandler (MouseEvent.MOUSE_CLICKED,
+			event -> ((Stage) rootPane.getScene ().getWindow ()).setIconified (true));
+		this.alwaysOnTopIcon.addEventHandler (MouseEvent.MOUSE_CLICKED, (event) -> {
+			boolean newVal = ! this.stage.isAlwaysOnTop ();
+			this.alwaysOnTopIcon.pseudoClassStateChanged (PseudoClass.getPseudoClass ("always-on-top"), newVal);
+			this.stage.setAlwaysOnTop (newVal);
+		});
+		this.hB_Header.setOnMousePressed ((event) -> {
+			this.xOffset = this.stage.getX () - event.getScreenX ();
+			this.yOffset = this.stage.getY () - event.getScreenY ();
+		});
+		this.hB_Header.setOnMouseDragged ((event) -> {
+			this.stage.setX (event.getScreenX () + this.xOffset);
+			this.stage.setY (event.getScreenY () + this.yOffset);
+		});
+		this.initializeLoader ();
+		ScrollUtils.addSmoothScrolling (this.scrollPane);
+	}
+	
+	private void initializeLoader () {
+		MFXLoader loader = new MFXLoader ();
+		
+		loader.addView (MFXLoaderBean.of ("Nueva Venta",
+			ResourcesLoader.loadURL ("view/NuevaVenta_view.fxml")).setBeanToNodeMapper (() -> {
+			return this.createToggle ("mfx-folder", "Nueva Venta");
+		}).setDefaultRoot (true).get ());
+		
+		loader.setOnLoadedAction ((beans) -> {
+			List<ToggleButton> nodes = beans.stream ().map ((bean) -> {
+				ToggleButton toggle = (ToggleButton) bean.getBeanToNodeMapper ().get ();
+				toggle.setOnAction ((event) -> {
+					this.contentPane.getChildren ().setAll (bean.getRoot ());
+				});
+				if (bean.isDefaultView ()) {
+					this.contentPane.getChildren ().setAll (bean.getRoot ());
+					toggle.setSelected (true);
+				}
+				
+				return toggle;
+			}).toList ();
+			this.navBar.getChildren ().setAll (nodes);
+		});
+		
+		
+		loader.start ();
+	}
+	
+	private ToggleButton createToggle (String icon, String text) {
+		MFXIconWrapper wrapper = new MFXIconWrapper (icon, 24.0, 32.0);
+		MFXRectangleToggleNode toggleNode = new MFXRectangleToggleNode (text, wrapper);
+		toggleNode.setAlignment (Pos.CENTER_LEFT);
+		toggleNode.setToggleGroup (this.toggleGroup);
+		
+		return toggleNode;
 	}
 	
 	public void closeWindows () {
