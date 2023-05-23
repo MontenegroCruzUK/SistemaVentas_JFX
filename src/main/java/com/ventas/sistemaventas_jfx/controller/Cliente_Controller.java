5ae8a886c6149010b550a5cbf4dfb8e1b430a8c7
MontenegroCruzUK <montenegrocruz.uk@gmail.com>
2023-05-23 14:21:42 +0100

    Tenemos problemas con el método actualizar.

--- src/main/java/com/ventas/sistemaventas_jfx/controller/Cliente_Controller.java
+++ src/main/java/com/ventas/sistemaventas_jfx/controller/Cliente_Controller.java
@@ -3,6 +3,8 @@ package com.ventas.sistemaventas_jfx.controller;
 import com.ventas.sistemaventas_jfx.model.Cliente;
 import com.ventas.sistemaventas_jfx.model.ClienteDado;
 import com.ventas.sistemaventas_jfx.model.Mensajes;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -11,15 +13,18 @@ import javafx.fxml.Initializable;
 import java.util.List;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 
 public class Cliente_Controller implements Initializable {
 	Cliente cliente = new Cliente ();
@@ -89,14 +94,99 @@ public class Cliente_Controller implements Initializable {
 		tc_RazonSocial.setCellValueFactory (cellData -> cellData.getValue ().getRazonSocial ());
 		ClienteDado clienteDado = new ClienteDado ();
 		List listaClientes = clienteDado.listarClientes ();
-		tbl_Clientes.getItems ().addAll (listaClientes);
+		tbl_Clientes.getItems ().setAll (listaClientes);
+		tbl_Clientes.setOnMouseClicked (this::seleccionarCamposTablaClientes);
 		
+	}
+	
+	private void seleccionarCamposTablaClientes (MouseEvent event) {
+		cliente = tbl_Clientes.getSelectionModel ().getSelectedItem ();
+		if (cliente != null) {
+			txt_Id.setText (String.valueOf (cliente.getId_cliente ().get ()));
+			txt_Dni.setText (cliente.getDni ().get ());
+			txt_Nombre.setText (cliente.getNombre ().get ());
+			txt_Telefono.setText (cliente.getTelefono ().get ());
+			txt_Direccion.setText (cliente.getDireccion ().get ());
+			txt_RazonSocial.setText (cliente.getRazonSocial ().get ());
+		}
 	}
 	
 	@FXML
 	public void agregarNuevoCliente (ActionEvent event) {
-		System.out.println ("Estamos trabajando en ello");
+		String dni = txt_Dni.getText ();
+		String nombre = txt_Nombre.getText ();
+		String telefono = txt_Telefono.getText ();
+		String direccion = txt_Direccion.getText ();
+		String razonSocial = txt_RazonSocial.getText ();
+		
+		if (! dni.isEmpty () || ! nombre.isEmpty () || ! telefono.isEmpty () || ! direccion.isEmpty () ||
+			! razonSocial.isEmpty ()) {
+			cliente.setDni (new SimpleStringProperty (dni));
+			cliente.setNombre (new SimpleStringProperty (nombre));
+			cliente.setTelefono (new SimpleStringProperty (telefono));
+			cliente.setDireccion (new SimpleStringProperty (direccion));
+			cliente.setRazonSocial (new SimpleStringProperty (razonSocial));
+			clienteDado.registrarNuevoCliente (cliente);
+			System.out.println ("Se registró correctamente");
+			cargarDatosClientes ();
+			limpiarCampos ();
+		} else {
+			System.out.println ("Fallo en el registro");
+		}
+	}
+	
+	private void limpiarCampos () {
+		txt_Dni.setText ("");
+		txt_Nombre.setText ("");
+		txt_Telefono.setText ("");
+		txt_Direccion.setText ("");
+		txt_RazonSocial.setText ("");
 	}
 	
 	
+	public void eliminarCliente (ActionEvent actionEvent) {
+		
+		if (! txt_Id.getText ().isEmpty ()) {
+			Alert alert = new Alert (Alert.AlertType.CONFIRMATION);
+			alert.setTitle ("Confirmar eliminación");
+			alert.setHeaderText (null);
+			alert.setContentText ("¿Está seguro de eliminar?");
+			
+			Optional<ButtonType> result = alert.showAndWait ();
+			if (result.isPresent () && result.get () == ButtonType.OK) {
+				int id = Integer.parseInt (txt_Id.getText ());
+				System.out.println ("Este es el Id = " + id);
+				clienteDado.eliminarCliente (id);
+				limpiarCampos ();
+				cargarDatosClientes ();
+			}
+		}
+	}
+	
+	public void actualizarCliente (ActionEvent actionEvent) {
+		if (txt_Id.getText ().isEmpty ()) {
+			Alert alert = new Alert (Alert.AlertType.WARNING);
+			alert.setTitle ("Advertencia");
+			alert.setHeaderText (null);
+			alert.setContentText ("Seleccione una fila");
+			alert.showAndWait ();
+		} else {
+			if (! txt_Dni.getText ().isEmpty () || ! txt_Nombre.getText ().isEmpty () ||
+				! txt_Telefono.getText ().isEmpty () || ! txt_Direccion.getText ().isEmpty () ||
+				! txt_RazonSocial.getText ().isEmpty ()) {
+				
+				cliente.setDni (new SimpleStringProperty (cliente.getDni ().get ()));
+				cliente.setDni (new SimpleStringProperty (cliente.getNombre ().get ()));
+				cliente.setDni (new SimpleStringProperty (cliente.getTelefono ().get ()));
+				cliente.setDni (new SimpleStringProperty (cliente.getDireccion ().get ()));
+				cliente.setDni (new SimpleStringProperty (cliente.getRazonSocial ().get ()));
+				cliente.setId_cliente (new SimpleIntegerProperty (cliente.getId_cliente ().get ()));
+				clienteDado.modificarCliente (cliente);
+				cargarDatosClientes ();
+			} else {
+				System.out.println ("Los campos están vacíos");
+			}
+		}
+		
+	}
 }
--- src/main/java/com/ventas/sistemaventas_jfx/model/ClienteDado.java
+++ src/main/java/com/ventas/sistemaventas_jfx/model/ClienteDado.java
@@ -1,5 +1,6 @@
 package com.ventas.sistemaventas_jfx.model;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -34,7 +35,64 @@ public class ClienteDado {
 			throw new RuntimeException (e);
 		}
 		return listarClientes;
+	}
+	
+	public boolean registrarNuevoCliente (Cliente cliente) {
+		String sql = "INSERT INTO clientes (dni, nombre, telefono, direccion, razon_social)VALUES (?,?,?,?,?)";
+		try {
+			connection = conexion.getConnection ();
+			ps = connection.prepareStatement (sql);
+			
+			ps.setString (1, cliente.getDni ().get ());
+			ps.setString (2, cliente.getNombre ().get ());
+			ps.setString (3, cliente.getTelefono ().get ());
+			ps.setString (4, cliente.getDireccion ().get ());
+			ps.setString (5, cliente.getRazonSocial ().get ());
+			ps.execute ();
+			System.out.println ("Se pudo registrar al cliente");
+			return true;
+			
+		} catch (SQLException e) {
+			System.out.println ("No se pudo registrar al cliente");
+			return false;
+		}
+	}
+	
+	public boolean eliminarCliente (int id) {
+		System.out.println ("aqui tienes tu Id " + id);
+		String sql = "DELETE FROM clientes  WHERE id_cliente = ?";
+		try {
+			connection = conexion.getConnection ();
+			ps = connection.prepareStatement (sql);
+			ps.setInt (1, id);
+			ps.execute ();
+			System.out.println ("Se elimino correctamente");
+			return true;
+		} catch (SQLException e) {
+			System.out.println ("No se puede eliminar");
+			return false;
+		}
+	}
+	
+	public boolean modificarCliente (Cliente cliente) {
+		String sql = "UPDATE clientes SET dni=?, nombre=?, telefono=?, direccion=?, razon_social=? WHERE id_cliente=?";
 		
+		try {
+			connection = conexion.getConnection ();
+			ps = connection.prepareStatement (sql);
+			ps.setString (1, String.valueOf (cliente.getDni ()));
+			ps.setString (2, String.valueOf (cliente.getNombre ()));
+			ps.setString (3, String.valueOf (cliente.getDireccion ()));
+			ps.setString (5, cliente.getRazonSocial ().get ());
+			ps.setInt (6, cliente.getId_cliente ().get ());
+			ps.executeUpdate ();
+			System.out.println ("Se modifico correctamente");
+			return true;
+		} catch (SQLException e) {
+			System.out.println ("No se puede modificar\n" + e.toString ());
+			
+			return false;
+		}
 	}
 }
 
